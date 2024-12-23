547ea881d4d53c58e1f50da7be4d4266075ccaa9
Gero <gerojbro@gmail.com>
2024-12-23 19:13:28 -0300

    Estethics

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -1,13 +1,24 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock, faPhoneAlt, faMapMarkerAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./ContactForm.css";
 
 const ContactForm = () => {
   return (
     <div className="contact-page">
+     <div className='contact-style'>
       <header className="header">
-        <h1>Contacto</h1>
-        <p>Si tenés dudas o consultas, escribinos.</p>
-      </header>
+        <h1 className='half-text'>
+      <div className="top"></div>
+      <div className="bottom"></div>
+      Contacto
+    </h1>
+    <p>Si tenés dudas o consultas, escribinos.</p>
+  </header>
+</div>
+      
+      <div className='d-flex align-items-center'>
 
       <div className="container my-5">
         <div className="row">
@@ -75,37 +86,39 @@ const ContactForm = () => {
           </div>
 
           {/* Información adicional */}
-          <div className="col-md-6">
+          <div className="col-md-6 offset-md-8"style={{ marginTop: '-630px' }}>
             <ul className="list-unstyled">
               <li className="mb-3">
-                <strong>Horario:</strong> Lunes a viernes de 9 a 17 hs.
+                <FontAwesomeIcon icon={faClock}  className="icon-custom"/> <strong>Horario:</strong> Lunes a viernes de 9 a 17 hs.
               </li>
               <li className="mb-3">
-                <strong>Teléfono:</strong> +54 9 341 5767374
+                <FontAwesomeIcon icon={faPhoneAlt} className="icon-custom" /> <strong>Teléfono:</strong> +54 9 341 5767374
               </li>
               <li className="mb-3">
-                <strong>Dirección:</strong> Moreno 1623 3º “A”, Rosario, Santa Fe
+                <FontAwesomeIcon icon={faMapMarkerAlt} className="icon-custom" /> <strong>Dirección:</strong> Moreno 1623 3º “A”, Rosario, Santa Fe
               </li>
               <li className="mb-3">
-                <strong>Correo:</strong> sassoinmobiliaria@gmail.com
+                <FontAwesomeIcon icon={faEnvelope} className="icon-custom" /> <strong>Correo:</strong> sassoinmobiliaria@gmail.com
               </li>
               <li className="mb-3">
+                <div className="social-icons mt-8">
                 <strong>Síguenos:</strong>
                 <ul className="list-inline">
                   <li className="list-inline-item">
-                    <a href="#">Facebook</a>
+                    <a href="#"><FontAwesomeIcon icon={faFacebook} className="icon-custom" /></a>
                   </li>
                   <li className="list-inline-item">
-                    <a href="#">Instagram</a>
+                    <a href="#"><FontAwesomeIcon icon={faInstagram} className="icon-custom" /></a>
                   </li>
                 </ul>
+                </div>
               </li>
             </ul>
           </div>
         </div>
       </div>
+      </div>
 
-      {/* Mapa */}
       <div className="map-container">
         <iframe
           src="https://www.google.com/maps/embed/v1/place?key=AIzaSyB0s_yzzkRZVOUEWyL7y7lmd4qA7ygiIlc&q=Moreno+1623+Rosario+Santa+Fe+Argentina"
--- src/Components/navBar/NavBar.jsx
+++ src/Components/navBar/NavBar.jsx
@@ -4,24 +4,37 @@ const NavBar = () => {
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-light custom-navbar">
-                <div className="d-flex justify-content-between bg-dark w-100">
-                    <div className="d-flex">
-                        <img width={60} height={60} className="bg-success" alt="Logo" />
+                <div 
+                    className="d-flex justify-content-between w-100" 
+                    style={{ backgroundColor: 'rgb(11, 47, 53)' }}
+                >
+                    <div className="d-flex" style={{ backgroundColor: 'rgb(11, 47, 53)', padding: '10px', borderRadius: '10px' // Opcional, redondea bordes
+    }}>
+                        <img 
+                            src="/LogoSassoNuevo.png" 
+                            width={130} 
+                            height={110} 
+                            className="bg-success" 
+                            alt="Logo" 
+                        />
                     </div>
-                    <div className="d-flex justify-content-end bg-primary w-100 p-4 pe-5">
-                        <Link to="/propList" className="btn btn-primary text-white me-2">
+                    <div 
+                        className="d-flex justify-content-end w-100 p-4 pe-5" 
+                        style={{ backgroundColor: 'rgb(11, 47, 53)' }} 
+                    >
+                        <Link to="/propList" className="btn btn-primary text-white me-2" style={{ backgroundColor: 'rgb(11, 47, 53)' }}>
                             Propiedades
                         </Link>
-                        <Link to="/appraisalForm" className="btn btn-primary text-white me-2">
+                        <Link to="/appraisalForm" className="btn btn-primary text-white me-2" style={{ backgroundColor: 'rgb(11, 47, 53)' }}>
                             Tasaciones
                         </Link>
-                        <Link to="/contact" className="btn btn-primary text-white pe-5">
+                        <Link to="/contact" className="btn btn-primary text-white pe-5" style={{ backgroundColor: 'rgb(11, 47, 53)' }}>
                             Contacto
                         </Link>
                     </div>
                 </div>
                 <div className="py-1" />
-                <div className="py-3 bg-warning" />
+                <div className="py-3" style={{ backgroundColor: 'rgb(255, 193, 7)' }} />
             </nav>
         </>
     );
